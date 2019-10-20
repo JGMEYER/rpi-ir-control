b733fe5f9f71957d1f3de8714f0cf0fcb5cd6849
JGMEYER <meyer.justian@gmail.com>
2019-10-20 00:17:11 -0400

    Re-standardize on/off readings and convert to binary

--- src/ir.py
+++ src/ir.py
@@ -15,10 +15,10 @@ class IR_Receiver():
     MAX_READ_PULSE = 65 * 0.001
 
     # timing resolution for pulse reads
-    PULSE_RESOLUTION = 5 * 0.000001
+    PULSE_RESOLUTION = 10 * 0.000001
 
     def __init__(self):
-        self.sensor = InputDevice(16, pull_up=True)
+        self.sensor = InputDevice(16)
         self.led = LED(26)
 
     def read_loop(self):
@@ -33,37 +33,48 @@ class IR_Receiver():
                 # occasionally we'll get rogue pulses. ignore these
                 if len(pulses) > 2:
                     self._print_pulses(pulses)
+                    print(self._pulses_to_binary(pulses))
                 pulses = []
 
         while 1:
-            high_pulse = self._sense_pulse(True)
-            track_pulse(high_pulse, True)
-            low_pulse = self._sense_pulse(False)
-            track_pulse(low_pulse, False)
+            high_pulse = self._sense_pulse(False)
+            track_pulse(high_pulse, False)
+            low_pulse = self._sense_pulse(True)
+            track_pulse(low_pulse, True)
 
     def _sense_pulse(self, active):
         pulse = 0
-        while self.sensor.is_active == active:
+        while self.sensor.is_active is active:
             # pulse has timed out, return nothing
             if pulse >= self.MAX_READ_PULSE:
                 return 0
-            self.led.on() if active else self.led.off()
+            self.led.on() if not active else self.led.off()
             pulse += self.PULSE_RESOLUTION
             time.sleep(self.PULSE_RESOLUTION)
         return pulse
 
     def _print_pulses(self, pulses):
-        if pulses[0][1] is not False:
+        if pulses[0][1] is not True:
             print("Pulses does not start with a LOW pulse")
             return
         if len(pulses) % 2 != 0:
             print("Pulses does not have matching pairs of (LOW, HIGH) pulses")
             return
 
-        print("{0:10} {1:10}".format("OFF", "ON"))
+        print("{0:10} {1:10}".format("OFF (1)", "ON (0)"))
         for idx in range(0, len(pulses), 2):
             print("{0:7.2f} usecs {1:7.2f} usecs".format(pulses[idx][0],
                                                          pulses[idx+1][0]))
+
+    def _pulses_to_binary(self, pulses):
+        pulse_bin = ""
+        # ignore first and last pulse, then read in pairs
+        for idx in range(1, len(pulses)-1, 2):
+            if pulses[idx+1][0] / pulses[idx][0] >= 2:
+                pulse_bin += "1"
+            else:
+                pulse_bin += "0"
+        return pulse_bin
 
 
 if __name__ == "__main__":
